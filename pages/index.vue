cb1eee1c3ca7ccf75e0a60dd40c60e5794c3e611
Anitasilvi <anitaferdina06@gmail.com>
2023-09-24 09:49:28 +0700

    add components header & footer

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+    <section>
+        <h1 class="text-5xl font-bold mt-20">Hello, I'm Anita Silvi Ferdina</h1>
+        <p class="text-base text-gray-900 p-2 italic">Web Developer</p>
+    </section>
+    <section class="flex flex-col md:flex-row">
+        <div class="md:w-3/4">
+            <h2 class="text-3xl font-bold mt-8">My Story</h2>
+            <p class="text-lg py-2">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat saepe fuga cupiditate itaque accusamus officia laudantium sint quo laborum eius architecto rerum veritatis, rem praesentium nihil pariatur mollitia iure laboriosam.
+            </p>
+            <p class="text-lg py-2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet cum voluptas alias. Dignissimos fugit exercitationem odio eum modi placeat laudantium possimus odit excepturi reprehenderit! Eos numquam ipsa hic illo repudiandae.
+            </p>
+            <p class="text-lg py-2">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat saepe fuga cupiditate itaque accusamus officia laudantium sint quo laborum eius architecto rerum veritatis, rem praesentium nihil pariatur mollitia iure laboriosam.
+            </p>
+        </div>
+    </section>
+    <!-- <img src="../assets/images/128.jpg" class="w-1/2 md:max-w-sm p-8 mx-auto"> -->
+    <section>
+        <h2 class="text-3xl font-bold mt-8">Latest Blog Posts</h2>
+    </section>
+</template>
